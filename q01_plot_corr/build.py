977a1cc60f15e93b210b64b297943f493d083e1e
yuvrajkale87 <yuvraj.kale87@gmail.com>
2018-01-26 13:16:13 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -6,3 +6,10 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your solution here:
+import matplotlib.pyplot as plt
+import seaborn as sns
+def plot_corr(data,size=11):
+    plt.figure(figsize=(15,12))
+    cmap = sns.diverging_palette(220, 20, as_cmap=True)
+    sns.heatmap(data.corr(),cmap=cmap)
+    plt.show()
